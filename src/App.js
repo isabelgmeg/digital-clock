9ab066f0838d518cb977f9f2b157e5ed5c81d490
isabelgmeg <71519606+isabelgmeg@users.noreply.github.com>
2021-07-20 12:02:45 +0200

    coords working and animation implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+
+import Clock from "./Clock";
 
 function App() {
+  const a = ["a", 0, 90];
+  const b = ["b", 0, 180];
+  const c = ["c", 180, 90];
+  const d = ["d", 270, 90];
+  const e = ["e", 270, 0];
+  const f = ["f", 270, 180];
+  const g = ["g", 315, 315];
+  const h = ["h", 135, 135];
+
+  const zero = [
+    a,
+    b,
+    b,
+    c,
+    d,
+    a,
+    c,
+    d,
+    d,
+    d,
+    d,
+    d,
+    d,
+    d,
+    d,
+    d,
+    d,
+    e,
+    f,
+    d,
+    e,
+    b,
+    b,
+    f,
+  ];
+
+  const one = [
+    a,
+    b,
+    c,
+    h,
+    e,
+    c,
+    d,
+    h,
+    g,
+    d,
+    d,
+    h,
+    g,
+    d,
+    d,
+    h,
+    a,
+    f,
+    e,
+    c,
+    e,
+    b,
+    b,
+    f,
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="clock-wrap">
+          <section className="number">
+            {zero.map((coord) => {
+              return <Clock hourDegrees={coord[1]} minuteDegrees={coord[2]} />;
+            })}
+          </section>
+
+          <section className="number">
+            {one.map((coord) => {
+              return <Clock hourDegrees={coord[1]} minuteDegrees={coord[2]} />;
+            })}
+          </section>
+        </div>
+      </main>
     </div>
   );
 }
